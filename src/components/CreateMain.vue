2792573221de9f4ec26ecb740723003f2d54c043
Evan <evan.c.miao@gmail.com>
2019-09-06 23:45:40 +0800

    Update Components

--- src/components/CreateMain.vue
+++ src/components/CreateMain.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="container">
+    <!-- 面包屑导航 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>企业工作台</el-breadcrumb-item>
       <el-breadcrumb-item>采购报价看板管理</el-breadcrumb-item>
       <el-breadcrumb-item>发布物料</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 分割线 -->
     <el-divider></el-divider>
+    <!-- Tab 切换 -->
     <el-tabs type="border-card">
       <el-tab-pane label="发布物料">
+        <!-- 表单 -->
         <el-form :model="form" :rules="rules" ref="form" label-width="100px">
+          <!-- 加背景色的盒子 -->
           <div class="form-part">
+            <!-- 应用类 input -->
             <el-form-item label="应用类" prop="use">
               <el-col :span="10">
                 <el-input v-model="form.use"></el-input>
               </el-col>
             </el-form-item>
+            <!-- 物料类-->
             <el-form-item label="物料类" prop="class" :inline="true">
+              <!-- disabled 选择框 -->
               <el-select v-model="form.class0" disabled></el-select>
+              <!-- 三级联动下拉选择框 -->
               <el-select v-model="form.class1" placeholder="请选择大类" @change="changeClass1">
                 <el-option v-for="item in form.classList" :key="item.id" :value="item.value"></el-option>
               </el-select>
@@ -27,11 +36,13 @@
                 <el-option v-for="item in form.classList3" :key="item.id" :value="item.value"></el-option>
               </el-select>
             </el-form-item>
+            <!-- 物料名称 input -->
             <el-form-item label="物料名称" prop="name">
               <el-col :span="10">
                 <el-input v-model="form.name"></el-input>
               </el-col>
             </el-form-item>
+            <!-- 截止日期 -->
             <el-form-item prop="date" label="截止日期" required>
               <el-col :span="10">
                 <el-date-picker
@@ -43,8 +54,11 @@
               </el-col>
             </el-form-item>
           </div>
+          <!-- 加背景色的盒子 -->
           <div class="form-part">
+            <!-- 结算方式 -->
             <el-form-item label="结算方式" prop="resource">
+              <!-- 单选按钮组 -->
               <el-radio-group v-model="form.resource" @change="changeRadio">
                 <el-col class="radio-item">
                   <el-radio label="现金电汇"></el-radio>
@@ -54,23 +68,29 @@
                 </el-col>
                 <el-col class="radio-item">
                   <el-radio label="预付定金"></el-radio>
+                  <!-- 定金百分比 -->
                   <el-input size="mini" class="mini-width" type="number"></el-input>
                   <span class="radio-text">%；剩余货款</span>
+                  <!-- 还款日期 -->
                   <el-date-picker type="date" class="mini-width" size="mini"></el-date-picker>
                   <span class="radio-text">支付</span>
                 </el-col>
                 <el-col class="radio-item">
                   <el-radio label="账期"></el-radio>
+                  <!-- 分期天数 -->
                   <el-date-picker type="date" class="mini-width" size="mini"></el-date-picker>
                   <span class="radio-text">天</span>
+                  <!-- 付款方式 -->
                   <el-select size="mini" placeholder="请选择付款方式" v-model="form.pay"></el-select>
                 </el-col>
               </el-radio-group>
             </el-form-item>
           </div>
+          <!-- 发布按钮 -->
           <el-button type="primary" @click="submitForm('form')">发布</el-button>
         </el-form>
       </el-tab-pane>
+      <!-- Tab 切换 第二页 -->
       <el-tab-pane label="批量添加">批量添加</el-tab-pane>
     </el-tabs>
   </div>
@@ -108,7 +128,9 @@ export default {
           { required: true, message: "请输入物料名称", trigger: "blur" },
           { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
         ],
-        class: [{ required: true, message: "请选择物料类", trigger: "change" }],
+        class: [
+          { required: false, message: "请选择物料类", trigger: "change" }
+        ],
         date: [
           {
             type: "date",
@@ -124,28 +146,63 @@ export default {
     };
   },
   methods: {
+    // 提交表单
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          // 创建一个对象 把表单的值赋给它
+          var newObj = {
+            use: this.form.use,
+            name: this.form.name,
+            status: "已上架"
+          };
+          // 获取三级联动的值并赋给新对象 从最小一级往上找
+          if (this.form.class3) {
+            newObj.class = this.form.class3;
+          } else if (this.form.class2) {
+            newObj.class = this.form.class2;
+          } else if (this.form.class1) {
+            newObj.class = this.form.class1;
+          } else {
+            newObj.class = this.form.class0;
+          }
+          // 将新对象添加到 localStorge
+          if (localStorage.tableData) {
+            var tableData = JSON.parse(localStorage.tableData);
+            tableData.unshift(newObj);
+            localStorage.tableData = JSON.stringify(tableData);
+          } else {
+            that = this;
+            // 如果 localStorge 中没有则请求本地数据 并添加到 localStorge
+            axios.get("static/tableData.json").then(function(response) {
+              if (response.status == 200) {
+                var tableData = JSON.stringify(response.data.unshift(newObj));
+                localStorage.setItem("tableData", tableData);
+              }
+            });
+          }
+           this.$router.push({path:'/manage'});
         } else {
           console.log("error submit!!");
           return false;
         }
       });
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    },
+    // 一级选择框 change 事件
     changeClass1(e) {
+      // 清空下级选择框 value 值
       this.form.class2 = "";
       this.form.class3 = "";
+      // 遍历数据
       for (var index in this.form.classList) {
+        // 找出当前选项
         if (e === this.form.classList[index].value) {
+          // 当前选项数据的 children 绑定子选择框
           this.form.classList2 = this.form.classList[index].children;
         }
       }
     },
+    // 二级选择框 change 事件
     changeClass2(e) {
       this.form.class3 = "";
       for (var index in this.form.classList2) {
@@ -155,13 +212,15 @@ export default {
       }
     },
     changeRadio(e) {
-      console.log(e)
+      console.log(e);
     }
   },
   mounted: function() {
     var that = this;
+    // 请求本地 JSON 数据
     axios.get("static/data.json").then(function(response) {
       if (response.status == 200) {
+        // 绑定到一级选择框
         that.form.classList = response.data;
       }
     });
--- src/components/ManageMain.vue
+++ src/components/ManageMain.vue
@@ -1,13 +1,201 @@
 <template>
-    
+  <div class="container">
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>企业工作台</el-breadcrumb-item>
+      <el-breadcrumb-item>采购报价看板管理</el-breadcrumb-item>
+      <el-breadcrumb-item>物料管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 分割线 -->
+    <el-divider></el-divider>
+    <!-- 表单 -->
+    <el-form :model="form" ref="form" label-width="0" size="mini">
+      <!-- 三级联动下拉选择框 -->
+      <el-form-item prop="class" :inline="true">
+        <el-select v-model="form.class1" placeholder="请选择大类" @change="changeClass1">
+          <el-option v-for="item in form.classList" :key="item.id" :value="item.value"></el-option>
+        </el-select>
+        <el-select v-model="form.class2" placeholder="请选择子类" @change="changeClass2">
+          <el-option v-for="item in form.classList2" :key="item.id" :value="item.value"></el-option>
+        </el-select>
+        <el-select v-model="form.class3" placeholder="请选择小类">
+          <el-option v-for="item in form.classList3" :key="item.id" :value="item.value"></el-option>
+        </el-select>
+        <!-- 状态 上架/下架 -->
+        <el-select v-model="form.status" placeholder="请选择状态">
+          <el-option :value="'已上架'"></el-option>
+          <el-option :value="'已下架'"></el-option>
+        </el-select>
+        <!-- 选择日期 -->
+        <el-date-picker type="date" placeholder="选择日期" v-model="form.date"></el-date-picker>
+        <el-input v-model="form.use" placeholder="请输入应用类/品牌/牌号/产地"></el-input>
+        <!-- 搜索按钮 -->
+        <el-button type="primary" @click="search('form')">搜索</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- 表格 -->
+    <el-table
+      :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+      style="width: 100%"
+      height="450"
+      size="mini"
+      border
+    >
+      <el-table-column prop="use" label="应用类" width="120" align="center"></el-table-column>
+      <el-table-column prop="class" label="物料类" width="120" align="center"></el-table-column>
+      <el-table-column prop="name" label="物料名称" width="240" align="center"></el-table-column>
+      <el-table-column prop="brand" label="品牌" width="120" align="center"></el-table-column>
+      <el-table-column prop="model" label="牌号" width="120" align="center"></el-table-column>
+      <el-table-column prop="format" label="规格" width="120" align="center"></el-table-column>
+      <el-table-column prop="address" label="产地" width="120" align="center"></el-table-column>
+      <el-table-column prop="status" label="状态" width="120" align="center"></el-table-column>
+      <el-table-column label="操作" width="240" align="center">
+        <template slot-scope="scope">
+          <el-button @click="delivery(scope.row)" type="text" size="small">上架/下架</el-button>
+          <el-button @click="copy(scope.row)" type="text" size="small">复制物料</el-button>
+          <el-button @click="edit(scope.row)" type="text" size="small">编辑</el-button>
+          <el-button @click="deleteItem(scope.row)" type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <div class="block">
+      <el-pagination layout="prev, pager, next" :total="50" @current-change="currentChange"></el-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name: "Manage"
-}
+  name: "Manage",
+  data() {
+    return {
+      form: {
+        use: "",
+        date: "",
+        status: "",
+        class1: "",
+        class2: "",
+        class3: "",
+        classList: "",
+        classList2: "",
+        classList3: ""
+      },
+      pageSize: 10,
+      currentPage: 1,
+      tableData: []
+    };
+  },
+  methods: {
+    changeClass1(e) {
+      this.form.class2 = "";
+      this.form.class3 = "";
+      for (var index in this.form.classList) {
+        if (e === this.form.classList[index].value) {
+          this.form.classList2 = this.form.classList[index].children;
+        }
+      }
+    },
+    changeClass2(e) {
+      this.form.class3 = "";
+      for (var index in this.form.classList2) {
+        if (e === this.form.classList2[index].value) {
+          this.form.classList3 = this.form.classList2[index].children;
+        }
+      }
+    },
+    search(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    // 上架/下架
+    delivery(e) {
+      if (e.status === "已下架") {
+        if (confirm("确定上架？")) e.status = "已上架";
+      } else {
+        if (confirm("确定下架？")) e.status = "已下架";
+      }
+    },
+    // 复制货物
+    copy(e) {
+      var newObj = {};
+      for (var key in e) {
+        // 将每个属性复制到 newObj 对象
+        newObj[key] = e[key];
+      }
+      this.tableData.unshift(newObj);
+    },
+    // 编辑
+    edit(e) {
+      console.log(e);
+    },
+    // 删除
+    deleteItem(e) {
+      for (var index in this.tableData) {
+        if (e === this.tableData[index]) {
+          this.tableData.splice(index, 1);
+          console.log(this.tableData);
+        }
+      }
+    },
+    // 分页
+    currentChange: function(currentPage) {
+      this.currentPage = currentPage;
+    }
+  },
+  mounted: function() {
+    var that = this;
+    axios.get("static/data.json").then(function(response) {
+      if (response.status == 200) {
+        that.form.classList = response.data;
+      }
+    });
+    // 从 localStorge 中取出数据
+    if (localStorage.tableData) {
+      this.tableData = JSON.parse(localStorage.tableData);
+    } else {
+      // 如果 localStorge 中没有则请求本地数据 并添加到 localStorge
+      axios.get("static/tableData.json").then(function(response) {
+        if (response.status == 200) {
+          // localStorge 只能存储字符串数据
+          var tableData = JSON.stringify(response.data);
+          localStorage.setItem("tableData", tableData);
+          // 绑定到模板的 tableData 属性
+          that.tableData = JSON.parse(localStorage.tableData);
+        }
+      });
+    }
+  },
+  // 侦听器
+  watch: {
+    // 侦听 tableData 的改变 并实时写入 localStorge
+    tableData(newTableData) {
+      localStorage.tableData = JSON.stringify(newTableData);
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.container {
+  padding: 0 40px;
+  .el-breadcrumb {
+    margin-top: 10px;
+  }
+  .el-select,
+  .el-input {
+    width: 120px;
+  }
+  .block {
+    margin: 10px auto;
+    text-align: center;
+  }
+}
 </style>
